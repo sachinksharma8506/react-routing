675cad2fb49aaa75f64536ba9bd8dc9d400c87fd
Sachin Sharma <sachin.k.sharma@globallogic.com>
2022-01-25 10:13:35 +0530

    Fix Routing and also css updated

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ function App() {
      <Routes>
        {/* <Route exact path = "/" element = {about}></Route>*/}
         <Route path = "/contact" element = {<Contact />}></Route>{/* We will use the Route Render method when we will partially update the component and need to pas some props. */}
-       <Route exact path="/" element = {() => <About pagename= "about" />}></Route> {/* We will use Render component method when we no need to pass any prop and when we need to create component every time. */}
+       <Route exact path="/" element = {<About pagename= "about" />}></Route> {/* We will use Render component method when we no need to pass any prop and when we need to create component every time. */}
        <Route path ="/routing" element = {Navbar}></Route>
-       <Route path ="/Search/:FName/:LName" element = {Search}></Route>
+       <Route path ="/Search/:FName/:LName" element = {<Search />}></Route>
+       {/* <Route path ="/Search/:FName/:LName" element = {Search}></Route> */}
        <Route element={error1}></Route>
      </Routes>
     </div>
